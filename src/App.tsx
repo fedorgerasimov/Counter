78b6435e391ab89baf32bef4e14fb239756ff958
fedorgerasimov <1031577@mail.ru>
2022-04-18 19:14:24 +0300

    added LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,81 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import s from './App.module.css'
 import {Button} from "./components/Button/Button";
 import "./components/Button/Button.module.css"
 import DisplayValue from "./components/DisplayValue/DisplayValue";
+import displayValue from "./components/DisplayValue/DisplayValue";
 
 
 function App() {
-  const [value, setValue] = useState(0)
+    const [startValue, setStartValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(0)
+    let [error, setError] = useState<string>('')
 
-  const Increment = () => {
-    setValue(value + 1)
-  }
+    const increment = () => {
+        setStartValue(startValue + 1)
+    }
 
-  const Reset = () => {
-    setValue(0)
-  }
+    const reset = () => {
+        setStartValue(0)
+    }
+
+    const inputStarValue = (value: number) => {
+        setStartValue(value)
+        if (startValue > value || startValue < 0) {
+            error = 'incorrect value'
+            setError(error)
+        } else setError('enter values and press "set"')
+    }
+
+    useEffect( ()=> {
+        let valueAsString = localStorage.getItem('startValue')  // вначале получаем значение и присваиваем переменной
+        if (valueAsString) {          //делаем проверку на NULL, затем нам нужно в useState отправить то что получили, для это делаем преобразование
+            let newValue = JSON.parse(valueAsString)   //JSON.parse  преобразовывает строку в объект
+            setStartValue(newValue)
+        }
+    }, [])
+
+    useEffect( ()=> {
+        localStorage.setItem('startValue', JSON.stringify(startValue))
+    }, [startValue])
+
+    useEffect( ()=> {
+        let valueAsString = localStorage.getItem('maxValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setStartValue(newValue)
+        }
+    }, [])
+
+    useEffect( ()=> {
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+    }, [maxValue])
+
+    const onClickStart = () => {
+        setStartValue(startValue+1)
+    }
+    const onClickMax = () => {
+        setMaxValue(maxValue+1)
+    }
 
 
-  return (
-      <div className={s.container}>
-        <DisplayValue value={value}/>
-        <div className={s.buttons_container}>
-          <Button disabled={value > 4} callback={Increment} name={'INC'}/>
-          <Button disabled={value === 0} callback={Reset} name={'RESET'}/>
+    return (
+        <div className={s.wrapper}>
+            <div className={s.Counter}>
+                <DisplayValue value={startValue}/>
+                <div className={s.buttons_container}>
+                    <Button disabled={startValue > 4} callback={increment} name={'INC'}/>
+                    <Button disabled={startValue === 0} callback={reset} name={'RESET'}/>
+                </div>
+            </div>
+            <div className={s.containerInput}>
+                <div>{startValue}</div>
+                <button onClick={onClickStart}>start</button>
+                <div>{maxValue}</div>
+                <button onClick={onClickMax}>max</button>
+            </div>
         </div>
-
-      </div>
-  )
+    )
 }
 
 export default App;
